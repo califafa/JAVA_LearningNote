ec06141ad5c98173a24256176df3f242e825cb43
cal <california.us@outlook.com>
2019-03-08 20:17:38 +0800

    add something about jdbc

--- jdbc/testJdbc01.java
+++ jdbc/testJdbc01.java
@@ -4,6 +4,21 @@ import java.sql.*;
 public class testJdbc01{
     public static void main(String[] args) throws Exception{
 
+        ResultSet rs1=Query1();
+        rs1.previous();//previous one
+        rs1.absolute(0);
+        while(rs1.next()){
+            System.out.println("row  "+rs1.getRow());
+        }
+
+        ResultSet rs2=Query2();
+        rs2.absolute(0);
+        while(rs2.next()){
+            System.out.println("row  "+rs2.getRow());
+        }
+    }
+
+    public static ResultSet Query1() throws Exception{        
         String Url="jdbc:mysql://localhost:3306/db01?serverTimezone=UTC";
         String User="root";
         String Passwd="MySQL";
@@ -11,12 +26,34 @@ public class testJdbc01{
         System.out.println("forName completed");
         Connection conn=DriverManager.getConnection(Url,User,Passwd);
         System.out.println("Connected");
-        Statement st=conn.createStatement();
-        //st.executeQuery("CREATE DATABASE db02");
-        //st.executeQuery("USE db01");
-        ResultSet rs=st.executeQuery("select * from users where id=1 || name='val'");
+        String mySql="select * from users where id=? || name=?";
+        PreparedStatement pst=conn.prepareStatement(mySql);
+        pst.setInt(1, 3);
+        pst.setString(2, "hel");
+        ResultSet rs=pst.executeQuery();
         while(rs.next()){
             System.out.println(rs.getString("id")+" - "+rs.getString("name"));
         }
+        return rs;
     }
+
+    public static ResultSet Query2() throws Exception{
+        String Url="jdbc:mysql://localhost:3306/db01?serverTimezone=UTC";
+        String User="root";
+        String Passwd="MySQL";
+        Class.forName("com.mysql.cj.jdbc.Driver");//.newInstance();
+        System.out.println("forName completed");
+        Connection conn=DriverManager.getConnection(Url,User,Passwd);
+        System.out.println("Connected");
+
+        Statement st=conn.createStatement();
+
+        ResultSet rs=st.executeQuery("select * from users where id=1 || name='val'");
+        
+        while(rs.next()){
+            System.out.println(rs.getString("id")+" - "+rs.getString("name"));
+        }
+        return rs;
+    }
+    
 }
